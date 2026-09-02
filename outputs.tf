3142b8c8c9b28a6b5a6962599efdd95daaed0b5c
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-09-02 13:43:08 +0530

    terraform vpc module development

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "vpc_info" {
   value = aws_vpc.main.id
 }
-
+output "public_subnets"{
+  value= aws_subnet.public[*].id
+}
+output "private_subnets"{
+  value= aws_subnet.private[*].id
+}
+output "database_subnets"{
+  value= aws_subnet.database[*].id
+}
 # output "az_info" {
 #   value = local.azs
 # }
