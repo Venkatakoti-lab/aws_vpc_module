e35feafa239dc012e8f4271560101ca2d01ba297
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-09-03 13:05:00 +0530

    terraform vpc module development

--- vpc.tf
+++ vpc.tf
@@ -31,6 +31,7 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = local.azs[count.index]
+  map_public_ip_on_launch= true
 
   tags = merge(
     var.public_subnet_tags,
